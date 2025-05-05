6a557717c86cea988022e6faaf55de748da704d4
Adhithya K <145216997+adhi2k@users.noreply.github.com>
2025-05-05 11:27:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// IMPORT THE PRODUCTS ROUTE
 const productRoutes = require('./routes.products');
 
-const app = express();
-const uri = "mongodb+srv://testadhithya:nmhLqmL268BpMRv0@cluster0.fosb8wj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// MIDDLEWARE
+app.use('/api/products', productRoutes);  // Use the route
 
-app.use('/api/products', productRoutes);
-
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
