8f1f4b0bf227e90954bb18132ac52a558aefc2d3
Adhithya K <145216997+adhi2k@users.noreply.github.com>
2025-05-05 11:27:51 +0530

    Update routes.products.js

--- routes.products.js
+++ routes.products.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('./models.Product');
+const products = require('./products.json');
 
-router.get('/', async (req, res) => {
-  const products = await Product.find();
+router.get('/', (req, res) => {
   res.json(products);
-});
-
-router.get('/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.json(product);
 });
 
 module.exports = router;
